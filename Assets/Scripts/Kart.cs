c6d33789ad9c841aecd2743370770a4e0f257440
Andrew <mrrobottrax@gmail.com>
2026-02-11 21:25:51 -0500

    Arduino read

--- Assets/Scripts/Kart.cs
+++ Assets/Scripts/Kart.cs
@@ -1,6 +1,7 @@
 using Unity.Hierarchy;
 using UnityEngine;
 using System.IO.Ports;
+using System;
 
 [RequireComponent (typeof(Rigidbody))]
 public class Kart : MonoBehaviour
@@ -12,9 +13,13 @@ public class Kart : MonoBehaviour
 	public bool isAccelerating = false;
 
 	private const float k_MoveSpeed = 10.0f;
-	private const float k_SteerFactor = 10.0f;
+	private const float k_SteerFactor = 50.0f;
 
 	private SerialPort myPort;
+
+	[SerializeField] string portname = "COM3";
+
+	byte buttons = 0;
 
 	void Start()
     {
@@ -22,6 +27,15 @@ public class Kart : MonoBehaviour
 		rb.useGravity = false;
 		rb.isKinematic = false;
 		rb.freezeRotation = true;
+
+		myPort = new SerialPort(portname, 9600);
+
+		myPort.Open();
+	}
+
+	void OnDestroy()
+	{
+		myPort.Close();
 	}
 
 	public void OnSteerInput()
@@ -36,7 +50,28 @@ public class Kart : MonoBehaviour
 
 	private void Update()
 	{
-		
+		while (myPort.BytesToRead > 0)
+		{
+			buttons = (byte)myPort.ReadByte();
+			buttons = (byte)~buttons;
+		}
+
+		Debug.Log(Convert.ToString(buttons, 2).PadLeft(8, '0'));
+
+		isAccelerating = (buttons & 1) != 0;
+
+		bool right = (buttons & 2) != 0;
+		bool left = (buttons & 4) != 0;
+
+		xInput = 0;
+
+		if (left) {
+			--xInput;
+		}
+
+		if (right) {
+			++xInput;
+		}
 	}
 
 	private void FixedUpdate()
@@ -45,11 +80,11 @@ public class Kart : MonoBehaviour
 		{
 			Quaternion rawRot = transform.rotation;
 			Vector3 rot = rawRot.eulerAngles;
-			Vector3 rotationVector = new Vector3(0, rot.y + (xInput * k_SteerFactor * Time.deltaTime), 0);
+			Vector3 rotationVector = new Vector3(0, rot.y + (xInput * k_SteerFactor * Time.fixedDeltaTime), 0);
 			rb.MoveRotation(Quaternion.Euler(rotationVector));
 
 			Vector3 moveDir = transform.forward;
-			rb.MovePosition(transform.position + (moveDir * k_MoveSpeed * Time.deltaTime));
+			rb.MovePosition(transform.position + (moveDir * k_MoveSpeed * Time.fixedDeltaTime));
 		}
 	}
 }
